f6ce86d62764328a98f6fc78fdee42609a3c809f
RuanCampello <ruancampello888@gmail.com>
2023-12-26 19:59:53 -0300

    fix: render error when there's no user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation'
 import { useContext, useEffect } from 'react'
 
 export default function Home() {
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext)
   const router = useRouter()
 
   useEffect(() => {
@@ -17,10 +17,12 @@ export default function Home() {
 
   return (
     <div className='w-screen h-screen bg-neutral-900 flex items-center justify-center'>
-      <div className='2xl:h-[95%] 2xl:w-5/6 2xl:rounded-lg w-full h-full bg-jet flex'>
-        <Sidebar />
-        <Chat />
-      </div>
+      { currentUser && 
+        <div className='2xl:h-[95%] 2xl:w-5/6 2xl:rounded-lg w-full h-full bg-jet flex'>
+          <Sidebar />
+          <Chat />
+        </div>
+      }
     </div>
   )
 }
