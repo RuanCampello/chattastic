aad4b8a1fa631eecef9b94fb2b0d568617f6cd37
RuanCampello <ruancampello888@gmail.com>
2023-12-29 14:49:03 -0300

    style: fix image empty space if there's no user

--- src/components/basicInfo.tsx
+++ src/components/basicInfo.tsx
@@ -6,7 +6,7 @@ interface BasicInfoProps {
 export default function BasicInfo({name, img}: BasicInfoProps) {  
   return (
   <div className='flex items-center gap-3'>
-     <img src={img} className='h-10 w-10 rounded-full object-cover' />
+     {img ? <img src={img} className='h-10 w-10 rounded-full object-cover' /> : <div className='h-10 w-10 rounded-full'></div>}
      <div className='flex flex-col'>
       <h2 className='text-lg leading-5 font-semibold'>{name}</h2>
     </div>
