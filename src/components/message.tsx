400a669c4ef5b89504906b3d4cd7758777b683eb
RuanCampello <ruancampello888@gmail.com>
2023-12-29 15:35:48 -0300

    style: image resize depeding of its height

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -8,7 +8,7 @@ interface MessageProps {
 export default function Message({owner, text, time, imgURL}: MessageProps) { 
   return (
     <div className={`${owner ? 'items-end' : 'items-start'} flex flex-col gap-1`}>
-      <div className={`max-w-[40%] p-3 leading-4 rounded-xl font-bold ${owner ? 'bg-neon-blue' : 'text-neutral-400 bg-eerie-black border border-neutral-600'} text-justify`}>
+      <div className={`max-w-[40%] p-3 leading-4 rounded-xl font-bold ${owner ? 'bg-neon-blue' : 'text-neutral-400 bg-eerie-black border border-neutral-600'} text-justify flex flex-col`}>
         {imgURL && <img className='w-auto object-cover h-48' src={imgURL} />}
         {text}
       </div>
