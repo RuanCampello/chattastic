2f717b87055e1d97b720fe7ac17a99cb3756c781
RuanCampello <ruancampello888@gmail.com>
2024-01-05 08:34:52 -0300

    fic: apostrophe typo

--- src/app/forgotpassword/page.tsx
+++ src/app/forgotpassword/page.tsx
@@ -13,7 +13,7 @@ export default function ForgotPassword() {
       <Form.Root onSubmitFunction={() => handleSubmit}>
         <Form.Header title='Forgot your password?'/>
         <p className='mb-4'>
-          Confirm your e-mail and we'll send you the instructions to reset password.
+          Confirm your e-mail and we&apos;ll send you the instructions to reset password.
         </p>
         <InputSection type='email' placeholder='E-mail' />
         <Form.Button title='Send e-mail'/>
