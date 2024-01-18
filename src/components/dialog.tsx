220808910a927ca3e2c115960a9b388d9f4d5333
RuanCampello <ruancampello888@gmail.com>
2024-01-18 15:17:14 -0300

    style: remove outline in opened image

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -17,7 +17,7 @@ export default function DialogWrapper({children, title, description, text, url}:
         {children}
       </Dialog.Trigger>
       <Dialog.Overlay className='fixed inset-0 bg-black bg-opacity-50 z-40' />
-      <Dialog.Content className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 rounded-xl'>
+      <Dialog.Content className='absolute focus:outline-none top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 rounded-xl'>
         <div className='bg-eerie-black py-1 px-3 rounded-t-xl'>
           <Dialog.Title className='text-neutral-200 text-lg font-bold'>{title}</Dialog.Title>
           <Dialog.Description className='text-neutral-400 text-sm font-semibold'>{description}</Dialog.Description>
